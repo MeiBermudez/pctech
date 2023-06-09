803abaf3fc741ad5230669b02204e10cce352ab8
MeiBermudez <bbermudesvanesa@gmail.com>
2023-06-08 21:46:56 -0600

    dashboardupdter

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -37,13 +37,13 @@
             height: 70%;
         }
     </style>
-    <title>Dashboard</title>
+    <title>panel administrativo</title>
 </head>
 
 <body>
 
     <div class="container">
-        <h1>Dashboard</h1>
+        <h1></h1>
 
         <div class="dashboard-container">
             <div class="dashboard-item">
@@ -56,6 +56,70 @@
                 <!-- Contenedor para el gráfico de informes de ventas -->
                 <div id="sales-chart" class="chart-container"></div>
             </div>
+            <div class="dashboard-item">
+                <h3>Pedidos</h3>
+                <!-- Contenido para la lista de pedidos -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Cliente</th>
+                            <th>Fecha</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Cliente 1</td>
+                            <td>2023-06-01</td>
+                            <td>$50.00</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Cliente 2</td>
+                            <td>2023-06-02</td>
+                            <td>$120.00</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Cliente 3</td>
+                            <td>2023-06-03</td>
+                            <td>$80.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="dashboard-item">
+                <h3>Productos más vendidos</h3>
+                <!-- Contenido para la lista de productos más vendidos -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Producto</th>
+                            <th>Cantidad Vendida</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Producto 1</td>
+                            <td>10</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Producto 2</td>
+                            <td>8</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Producto 3</td>
+                            <td>6</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
     </div>
@@ -65,49 +129,59 @@
         $(document).ready(function () {
             // Datos de ejemplo para el gráfico de visitas a la tienda
             var visitsData = {
-                series: [1200, 800, 1500, 900, 1300, 1100, 1700]
+                series: [1200]
             };
 
             // Opciones de configuración del gráfico de visitas a la tienda
             var visitsOptions = {
                 chart: {
-                    type: 'bar',
+                    type: 'radialBar',
                     height: 300,
                     sparkline: {
                         enabled: true
                     }
                 },
                 plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        columnWidth: '55%',
-                        endingShape: 'rounded'
-                    },
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                stroke: {
-                    show: true,
-                    width: 2,
-                    colors: ['transparent']
-                },
-                xaxis: {
-                    categories: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul'],
-                },
-                yaxis: {
-                    title: {
-                        text: 'Visitas'
+                    radialBar: {
+                        hollow: {
+                            size: '70%'
+                        },
+                        track: {
+                            margin: 15
+                        },
+                        dataLabels: {
+                            name: {
+                                fontSize: '16px',
+                                color: undefined,
+                                offsetY: 120
+                            },
+                            value: {
+                                fontSize: '36px',
+                                color: undefined,
+                                offsetY: 76,
+                                formatter: function (val) {
+                                    return val + " visitas";
+                                }
+                            }
+                        }
                     }
                 },
                 fill: {
-                    opacity: 1
+                    type: 'gradient',
+                    gradient: {
+                        shade: 'dark',
+                        shadeIntensity: 0.15,
+                        inverseColors: false,
+                        opacityFrom: 1,
+                        opacityTo: 1,
+                        stops: [0, 50, 53, 91]
+                    }
+                },
+                stroke: {
+                    dashArray: 4
                 },
                 colors: ['#FFD700'],
-                series: [{
-                    name: 'Visitas',
-                    data: visitsData.series
-                }]
+                series: visitsData.series
             };
 
             // Crear el gráfico de visitas a la tienda
@@ -148,7 +222,7 @@
                 },
                 yaxis: {
                     title: {
-                        text: 'Ventas'
+                        text: 'Ventas (USD)'
                     }
                 },
                 fill: {
